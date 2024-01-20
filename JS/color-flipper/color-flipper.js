25c679eb0ff6bdfcce6a542f3d0acedabeb2bd01
Ash <swannmerienne@gmail.com>
2024-01-20 19:06:16 +0100

    finished 1st project!!!

--- JS/color-flipper/color-flipper.js
+++ JS/color-flipper/color-flipper.js
@@ -1,1 +1,76 @@
+const box = document.getElementById("box");
+const boxColor = document.querySelector("#box");
 
+const text = document.getElementById("color");
+const textColor = document.querySelector("#color")
+
+
+function random_rgb() {
+    var o = Math.round, r = Math.random, s = 255;
+    return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + 1 + ')';
+}
+
+
+function flipcolor() {
+    let newColor = random_rgb();
+
+    boxColor.style.setProperty("background-color", newColor);
+    text.innerHTML = newColor.replace("a","").replace(/,[0-9]\)/, ")");
+}
+
+
+function colorToRGBA(color) {
+    // Returns the color as an array of [r, g, b, a] -- all range from 0 - 255
+    // color must be a valid canvas fillStyle. This will cover most anything
+    // you'd want to use.
+    // Examples:
+    // colorToRGBA('red')  # [255, 0, 0, 255]
+    // colorToRGBA('#f00') # [255, 0, 0, 255]
+    var cvs, ctx;
+    cvs = document.createElement('canvas');
+    cvs.height = 1;
+    cvs.width = 1;
+    ctx = cvs.getContext('2d');
+    ctx.fillStyle = color;
+    ctx.fillRect(0, 0, 1, 1);
+    return ctx.getImageData(0, 0, 1, 1).data;
+}
+
+
+function byteToHex(num) {
+    // Turns a number (0-255) into a 2-character hex number (00-ff)
+    return ('0'+num.toString(16)).slice(-2);
+}
+
+
+function colorToHex(color) {
+    // Convert any CSS color to a hex representation
+    // Examples:
+    // colorToHex('red')            # '#ff0000'
+    // colorToHex('rgb(255, 0, 0)') # '#ff0000'
+    var rgba, hex;
+    rgba = colorToRGBA(color);
+    hex = [0,1,2].map(
+        function(idx) { return byteToHex(rgba[idx]); }
+        ).join('');
+    return "#"+hex;
+}
+
+function getContrastYIQ(hexcolor){
+    var r = parseInt(hexcolor.substring(1,3),16);
+    var g = parseInt(hexcolor.substring(3,5),16);
+    var b = parseInt(hexcolor.substring(5,7),16);
+    var yiq = ((r*299)+(g*587)+(b*114))/1000;
+    return (yiq >= 128) ? 'black' : 'white';
+}
+
+
+function color() {
+    flipcolor();
+
+    let bgColor = colorToHex(box.style.backgroundColor);
+
+    console.log(bgColor);
+
+    textColor.style.color = getContrastYIQ(bgColor);
+}
